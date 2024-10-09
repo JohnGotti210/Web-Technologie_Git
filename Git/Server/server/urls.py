c2571a03904c5d18740192431f86028ac4b8fc9d
JohnGotti210 <marcel.commer98@web.de>
2024-10-09 18:12:58 +0200

    ordner fixed

--- Git/Server/server/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('startseite.urls')),  # Hier wird die App `katastrophen` hinzugefügt
-]
--- Git/Server/startseite/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import home, register_user, login_user
-from .views import register_user, login_user
-
-urlpatterns = [
-    path('', home, name='home'),  
-    path('register/', register_user, name='register'),
-    path('login/', login_user, name='login'),
-]
