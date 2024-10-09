554d259c428630db5a85cbb63ff091e565c04ae6
JohnGotti210 <marcel.commer98@web.de>
2024-10-09 17:57:46 +0200

    django new

--- Git/Server/server/views.py
+++ Git/Server/server/views.py
@@ -2,7 +2,7 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.contrib.auth.models import User
-from .serializers import UserSerializer
+from startseite.serializers import UserSerializer
 from django.contrib.auth import authenticate
 from rest_framework.authtoken.models import Token
 
